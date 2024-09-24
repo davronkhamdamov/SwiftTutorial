e8eb7500f2f684bd01aa403f0c8b3157689877af
davronxamdamov <xamdamovdavron6@gmail.com>
2024-09-24 07:39:11 +0500

    Initial Commit

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,32 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by Davron Xamdamov on 24/09/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct tutorialApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ tutorialTests/tutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialTests.swift
+//  tutorialTests
+//
+//  Created by Davron Xamdamov on 24/09/24.
+//
+
+import Testing
+@testable import tutorial
+
+struct tutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
